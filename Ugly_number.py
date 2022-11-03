5c8fbfb4b2e1677bb6048e4c37b41f806645c6cd
Mounika nagam <96118781+Mounikanagam@users.noreply.github.com>
2022-11-03 20:45:49 +0530

    def u(n):     t=[2,3,5]     for i in t:         while n%i==0:             n=n/i     return n==1 n=int(input()) if u(n)==1:     print("Ugly Number") else:     print("Not Ugly Number")

---
+++ Ugly_number.py
@@ -0,0 +1,11 @@
+def u(n):
+    t=[2,3,5]
+    for i in t:
+        while n%i==0:
+            n=n/i
+    return n==1
+n=int(input())
+if u(n)==1:
+    print("Ugly Number")
+else:
+    print("Not Ugly Number")
